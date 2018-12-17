dca2a240c7c9c577426c02852ce037a200d05d5f
Oleksandr.Petruk <oleksandr.petruk@fortegrp.net>
2018-12-17 19:48:09 +0200

    Implement Homework_8

--- Solution_For_Homework/Homework_8_Standalone/Homework_8_Standalone/Tests.cs
+++ Solution_For_Homework/Homework_8_Standalone/Homework_8_Standalone/Tests.cs
@@ -7,17 +7,20 @@ namespace Homework_8_Standalone
 {
     public class Tests
     {
+
         IWebDriver driver;
+        IJavaScriptExecutor jexec;
 
         [SetUp]
         public void SetUp()
         {
             //P.S. everything above should be executed headless.
             var chromeOptions = new ChromeOptions();
-            chromeOptions.AddArguments("headless", "start-maximized");
+            chromeOptions.AddArguments("start-maximized");//"headless",
             chromeOptions.AddUserProfilePreference("download.default_directory", Environment.SpecialFolder.Desktop);
 
             driver = new ChromeDriver(chromeOptions);
+            jexec = (IJavaScriptExecutor)driver;
         }
 
         [TearDown]
@@ -30,6 +33,18 @@ namespace Homework_8_Standalone
         public void Test_1()
         {
             driver.Url = "https://unsplash.com/search/photos/test";
+
+            var x = 0;
+            var y = 0;
+            var scrollYAmount = 200;
+
+            while (y != Convert.ToInt32(jexec.ExecuteScript("return window.pageYOffset")))
+            {
+                y += scrollYAmount;
+                jexec.ExecuteScript($"window.scrollBy({x}, {y})");
+            }
+
+            //Assert.True(promotionslabel.Displayed);
         }
     }
 }
